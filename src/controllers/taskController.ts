5f9d1de6d5cae11e6e69c4e88e6a6212e7152259
Ethan Almonte <ethan.almonte@students.mq.edu.au>
2026-02-11 23:38:18 +1100

    Added and tested the deleteTask function

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -96,3 +96,25 @@ export const updateTask = async (req: Request, res: Response) => {
         res.status(500).json({error: 'Server error'})
     }
 };
+
+export const deleteTask = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user?.userId;
+        const taskId = req.params.id;
+
+        if (!userId){
+            return res.status(401).json({ error: 'Unauthorized'})
+        }
+        const result = await pool.query(
+            'DELETE FROM tasks WHERE id = $1 AND user_id = $2 RETURNING *'
+            , [taskId, userId]
+        )
+            if(result.rows.length === 0){
+                return res.status(404).json({error: 'Task not found'})
+            }
+            res.status(200).json({message: 'Task deleted successfully'})
+    }catch (error) {
+        console.error("Error deleting task:", error);
+        res.status(500).json({error: 'Server error'})
+    }
+};
--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTask, getTasks, getTaskById, updateTask } from '../controllers/taskController';
+import { createTask, getTasks, getTaskById, updateTask, deleteTask } from '../controllers/taskController';
 import { authMiddleware } from '../middleware/auth.middleware';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/', authMiddleware, createTask);
 router.get('/', authMiddleware, getTasks);
 router.get('/:id', authMiddleware, getTaskById);
 router.put('/:id', authMiddleware, updateTask);
+router.delete('/:id', authMiddleware, deleteTask)
 
 export default router;
