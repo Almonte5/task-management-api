11ea61094ae1f8d8462a126b953019666d3c00d3
Ethan Almonte <ethan.almonte@students.mq.edu.au>
2026-02-11 23:24:27 +1100

    Added and tested the  updateTask function

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -71,3 +71,28 @@ export const getTaskById = async ( req: Request, res: Response) => {
         res.status(500).json({error: 'Server error'})
     }
 };
+
+export const updateTask = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user?.userId;
+        const taskId = req.params.id;
+        const { title, description, status, priority, due_date } = req.body;
+
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const result = await pool.query(
+            'UPDATE tasks SET title = $1, description = $2, status = $3, priority = $4, due_date = $5, updated_at = NOW() WHERE id = $6 AND user_id = $7 RETURNING *',
+            [title, description, status, priority, due_date, taskId, userId]
+        )
+        if(result.rows.length === 0){
+            return res.status(404).json({error: 'Task not found'})
+        }
+        res.status(200).json({task: result.rows[0]})
+
+    }catch (error) {
+        console.error("Error updating task:", error);
+        res.status(500).json({error: 'Server error'})
+    }
+};
--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTask, getTasks, getTaskById } from '../controllers/taskController';
+import { createTask, getTasks, getTaskById, updateTask } from '../controllers/taskController';
 import { authMiddleware } from '../middleware/auth.middleware';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/', authMiddleware, createTask);
 router.get('/', authMiddleware, getTasks);
 router.get('/:id', authMiddleware, getTaskById);
+router.put('/:id', authMiddleware, updateTask);
 
 export default router;
